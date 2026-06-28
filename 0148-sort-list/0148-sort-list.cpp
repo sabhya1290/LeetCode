4c6d4038828d59e0b3a5de1bd37c6ea6d4e78403
Sabhya Singh <sabhya1290@gmail.com>
2026-06-28 21:28:20 +0530

    Time: 9 ms (87.61%), Space: 56.9 MB (89.39%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,66 +1,55 @@
-							
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if(head == NULL || head ->next == NULL)
+        if(head == NULL || head->next == NULL){
             return head;
-        
-        
-        ListNode *temp = NULL;
-        ListNode *slow = head;
-        ListNode *fast = head;
-        
-        while(fast !=  NULL && fast -> next != NULL)
-        {
-            temp = slow;
-            slow = slow->next;         
-            fast = fast ->next ->next;  
-            
-        }   
-        temp -> next = NULL;            
-        
-        ListNode* l1 = sortList(head);    
-        ListNode* l2 = sortList(slow);
-        
-        return mergelist(l1, l2);         
-            
+        }
+        ListNode* L = head;
+        ListNode* mid = getmid(head);
+        ListNode* R = mid->next;
+        mid->next = NULL;
+        L = sortList(L);
+        R = sortList(R);
+        return merge(L,R);
     }
-    
-    ListNode* mergelist(ListNode *l1, ListNode *l2)
-    {
-        ListNode *ptr = new ListNode(0);
-        ListNode *curr = ptr;
-        
-        while(l1 != NULL && l2 != NULL)
-        {
-            if(l1->val <= l2->val)
-            {
-                curr -> next = l1;
-                l1 = l1 -> next;
-            }
-            else
-            {
-                curr -> next = l2;
-                l2 = l2 -> next;
-            }
-        
-        curr = curr ->next;
-        
+private:
+    ListNode* getmid(ListNode *head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        
-        
-        if(l1 != NULL)
-        {
-            curr -> next = l1;
-            l1 = l1->next;
-        }
-        
-        if(l2 != NULL)
-        {
-            curr -> next = l2;
-            l2 = l2 ->next;
-        }
-        
-        return ptr->next;
+        return slow;
     }
-};
+    ListNode* merge(ListNode *L, ListNode *R){
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        
+        while (L && R) {
+            if (L->val < R->val) {
+                tail->next = L;
+                L = L->next;
+            } else {
+                tail->next = R;
+                R = R->next;
+            }
+            tail = tail->next;
+        }
+        
+        // Append any remaining nodes from either list
+        tail->next = L ? L : R;
+        
+        return dummy.next;
+    }
+    };
