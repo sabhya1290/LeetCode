938435dfc760adc7c9a95edd89863a0fbffd12a4
Sabhya Singh <sabhya1290@gmail.com>
2026-06-29 20:53:33 +0530

    Time: 3 ms (34.49%), Space: 77.2 MB (45.04%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,30 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode();
-        ListNode* res = dummy;
-        int total = 0, carry = 0;
+        ListNode* ans = new ListNode(0, NULL);
+        ListNode* temp = ans; 
+        int carryforward = 0;
 
-        while (l1 || l2 || carry) {
-            total = carry;
-
-            if (l1) {
-                total += l1->val;
+        while(l1 != NULL || l2 != NULL){
+            if(l1 != NULL){
+                temp->val += l1->val;
                 l1 = l1->next;
             }
-            if (l2) {
-                total += l2->val;
+            if(l2 != NULL){
+                temp->val += l2->val;
                 l2 = l2->next;
             }
-
-            int num = total % 10;
-            carry = total / 10;
-            dummy->next = new ListNode(num);
-            dummy = dummy->next;
+            carryforward = (int) temp->val/10;
+            temp->val -= (carryforward*10);
+            
+            if((l1 != NULL || l2 != NULL) || carryforward != 0) temp->next = new ListNode(carryforward, NULL);
+            temp = temp->next;
         }
-
-        ListNode* result = res->next;
-        delete res;
-        return result;         
+        return ans;
     }
 };
