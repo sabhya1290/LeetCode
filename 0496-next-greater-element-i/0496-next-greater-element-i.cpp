1c4d63ee72911d2656ad6a00612a6eff5ec7034f
Sabhya Singh <sabhya1290@gmail.com>
2026-07-01 20:02:46 +0530

    Time: 0 ms (100%), Space: 12.8 MB (62.65%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,54 +1,21 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums2.size();
-        vector<int> ans(n);
-        ;
-        stack<int> helperStack;
-        for (int i = n - 1; i >= 0; i--) {
-            int currentElement = nums2[i];
+        unordered_map<int, int> next;
+        stack<int> s;
 
-            // if the stack is empty->no greater element
-            if (helperStack.empty()) {
-                ans[i] = -1;
-                helperStack.push(currentElement);
-                continue;
+        for (int i = nums2.size() - 1; i >= 0; --i) {
+            while (!s.empty() && s.top() <= nums2[i]) {
+                s.pop();
             }
-
-            // if top element of the stack is > nums2[i] then it is the next
-            // greater element
-            if (helperStack.top() > currentElement) {
-                ans[i] = helperStack.top();
-                helperStack.push(currentElement);
-                continue;
-            }
-
-            // remove all stack elements which are smaller than currentElement.
-            while (!helperStack.empty() && helperStack.top() < currentElement) {
-                helperStack.pop();
-            }
-
-            // if stack becomes empty after popping all smaller elements, then
-            // perform these three operations
-            if (helperStack.empty()) {
-                ans[i] = -1;
-            } else {
-                ans[i] = helperStack.top();
-            }
-            helperStack.push(currentElement);
-        }
-        unordered_map<int, int> mp;
-
-        for (int i = 0; i < n; i++) {
-            mp[nums2[i]] = ans[i];
+            next[nums2[i]] = s.empty() ? -1 : s.top();
+            s.push(nums2[i]);
         }
 
-        vector<int> result;
-
-        for (int i = 0; i < nums1.size(); i++) {
-            result.push_back(mp[nums1[i]]);
+        vector<int> res;
+        for (int num : nums1) {
+            res.push_back(next[num]);
         }
-
-        return result;
+        return res;
     }
 };
