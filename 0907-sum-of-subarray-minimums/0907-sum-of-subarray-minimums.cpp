3067edb62d5e7453b5b8486f31f3c6b02a89bb3d
Sabhya Singh <sabhya1290@gmail.com>
2026-07-02 20:23:46 +0530

    Time: 28 ms (57.66%), Space: 184.6 MB (49.95%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,59 +1,25 @@
 class Solution {
-private:
-    vector<int> nextSmallestElement(vector<int>& arr) {
-    int n = arr.size();
-    vector<int> nse(n, n);
-    stack<int> st;
-    for (int i = n - 1; i >= 0; i--) {
-        while (!st.empty() && arr[st.top()] >= arr[i]) {
-            st.pop();
-        }
-
-        if (!st.empty()) {
-            nse[i] = st.top();
-        }
-
-        st.push(i);
-    }
-
-    return nse;
-}
-
-    vector<int> prevSmallestElement(vector<int>& arr) {
-    int n = arr.size();
-    vector<int> pse(n, -1);
-    stack<int> st;
-
-    for (int i = 0; i < n; i++) {
-        while (!st.empty() && arr[st.top()] > arr[i]) {
-            st.pop();
-        }
-
-        if (!st.empty()) {
-            pse[i] = st.top();
-        }
-
-        st.push(i);
-    }
-
-    return pse;
-}
-
 public:
+    int mod=1e9+7;
     int sumSubarrayMins(vector<int>& arr) {
-    const int MOD = 1e9 + 7;
-    vector<int> nse = nextSmallestElement(arr);
-    vector<int> pse = prevSmallestElement(arr);
-
-    long long total = 0;
-
-    for (int i = 0; i < arr.size(); i++) {
-        long long left = i - pse[i];
-        long long right = nse[i] - i;
-
-        total = (total + (left * right % MOD) * arr[i]) % MOD;
+        int n=arr.size();
+        int ans=0;
+        vector<int> st;
+        vector<int> dp(n);
+        for(int i=0;i<n;i++){
+            while(!st.empty()&&arr[st.back()]>arr[i]){
+                st.pop_back();
+            }
+            st.push_back(i);
+            if(st.size()==1){
+             dp[i]=(arr[i])*(i+1);   
+            } else{
+                int val=st[st.size()-2];
+                dp[i]=(arr[i]*(i-val)+dp[val])%mod;
+            }
+            ans=(ans+dp[i])%mod;
+        }
+        return ans;
+        
     }
-
-    return total;
-}
 };
