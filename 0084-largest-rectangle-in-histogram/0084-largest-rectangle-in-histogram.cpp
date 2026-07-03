a7e43fc2138bbcb9d5b4a9a55031f28712301f11
Sabhya Singh <sabhya1290@gmail.com>
2026-07-03 17:51:26 +0530

    Time: 14 ms (87.39%), Space: 79.4 MB (96.44%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,32 +1,18 @@
-// C++
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> left(n), right(n);
-        stack<int> st;
-
-        // Nearest Smaller to Left
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && heights[st.top()] >= heights[i]) st.pop();
-            left[i] = st.empty() ? -1 : st.top();
+        int n = heights.size(),ans = 0 ;
+        stack<int> st; 
+        for(int i = 0 ; i <= n; i++){
+            while(!st.empty() && (i == n || heights[st.top()] >= heights[i])){
+                int mid = st.top();
+                st.pop();
+                int left = st.empty()? -1 : st.top();
+                left = i - left-1; 
+                ans = max(ans, left*heights[mid]);
+            }
             st.push(i);
         }
-
-        while (!st.empty()) st.pop();
-
-        // Nearest Smaller to Right
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && heights[st.top()] >= heights[i]) st.pop();
-            right[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
-
-        int maxArea = 0;
-        for (int i = 0; i < n; i++) {
-            int width = right[i] - left[i] - 1;
-            maxArea = max(maxArea, heights[i] * width);
-        }
-        return maxArea;
+        return ans ;
     }
 };
