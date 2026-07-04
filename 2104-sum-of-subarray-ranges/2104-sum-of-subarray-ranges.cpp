4df1306aac55a3af33d186df73b93c9b47b05926
Sabhya Singh <sabhya1290@gmail.com>
2026-07-04 15:30:25 +0530

    Time: 3 ms (73.68%), Space: 15.9 MB (43.92%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,107 +1,82 @@
 class Solution {
+    vector<int> nse(vector<int> & arr , int n ){
+        vector<int> v(n , n);
+        stack<int> st;
+        for(int i = n-1 ; i >= 0 ; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i] ){
+                st.pop();
+            }
+            if(!st.empty()){
+                v[i] = st.top();
+            }
+            st.push(i);
+        }
+        return v;
+
+    }
+    vector<int> pse(vector<int> & arr , int n ){
+        vector<int> v(n , -1);
+        stack<int> st;
+        for(int i = 0 ; i < n ; i++){
+            while(!st.empty() && arr[st.top()] > arr[i] ){
+                st.pop();
+            }
+            if(!st.empty()){
+                v[i] = st.top();
+            }
+            st.push(i);
+        }
+        return v;
+
+    }
+    vector<int> nge(vector<int> & arr , int n ){
+        vector<int> v(n , n);
+        stack<int> st;
+        for(int i = n-1 ; i >= 0 ; i--){
+            while(!st.empty() && arr[st.top()] <= arr[i] ){
+                st.pop();
+            }
+            if(!st.empty()){
+                v[i] = st.top();
+            }
+            st.push(i);
+        }
+        return v;
+
+    }
+    vector<int> pge(vector<int> & arr , int n ){
+        vector<int> v(n , -1);
+        stack<int> st;
+        for(int i = 0 ; i < n ; i++){
+            while(!st.empty() && arr[st.top()] < arr[i] ){
+                st.pop();
+            }
+            if(!st.empty()){
+                v[i] = st.top();
+            }
+            st.push(i);
+        }
+        return v;
+
+    }
 public:
-    vector<int> nse(const vector<int>& nums) {
-        int n = nums.size();
-        stack<int> st;
-        vector<int> arr(n, n);
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && nums[st.top()] >= nums[i]) {
-                st.pop();
-            }
-
-            if (!st.empty()) {
-                arr[i] = st.top();
-            }
-
-            st.push(i);
-        }
-
-        return arr;
-    }
-
-    vector<int> pse(const vector<int>& nums) {
-        int n = nums.size();
-        stack<int> st;
-        vector<int> arr(n, -1);
-
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && nums[st.top()] > nums[i]) {
-                st.pop();
-            }
-
-            if (!st.empty()) {
-                arr[i] = st.top();
-            }
-
-            st.push(i);
-        }
-
-        return arr;
-    }
-
-    vector<int> nge(const vector<int>& nums) {
-        int n = nums.size();
-        stack<int> st;
-        vector<int> arr(n, n);
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && nums[st.top()] <= nums[i]) {
-                st.pop();
-            }
-
-            if (!st.empty()) {
-                arr[i] = st.top();
-            }
-
-            st.push(i);
-        }
-
-        return arr;
-    }
-
-    vector<int> pge(const vector<int>& nums) {
-        int n = nums.size();
-        stack<int> st;
-        vector<int> arr(n, -1);
-
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && nums[st.top()] < nums[i]) {
-                st.pop();
-            }
-
-            if (!st.empty()) {
-                arr[i] = st.top();
-            }
-
-            st.push(i);
-        }
-
-        return arr;
-    }
-
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> n_se = nse(nums);
-        vector<int> p_se = pse(nums);
-        vector<int> n_ge = nge(nums);
-        vector<int> p_ge = pge(nums);
+        vector<int> pse1 = pse(nums , n);
+        vector<int> nse1 = nse(nums , n);
+        vector<int> nge1 = nge(nums , n);
+        vector<int> pge1 = pge(nums , n);
+        long long  total = 0 ;
 
-        long long largest = 0;
-        long long smallest = 0;
+        for(int i = 0 ; i < n ;i++){
+            int x = nse1[i] - i ; 
+            int y = i - pse1[i];
+            int z = nge1[i] - i ;
+            int w = i - pge1[i];
 
-        for (int i = 0; i < n; i++) {
-            long long maxCount =
-                1LL * (i - p_ge[i]) * (n_ge[i] - i);
-
-            long long minCount =
-                1LL * (i - p_se[i]) * (n_se[i] - i);
-
-            largest += maxCount * nums[i];
-            smallest += minCount * nums[i];
+            total += ((w * z*1LL) - (x * y*1LL))*nums[i];
         }
-
-        return largest - smallest;
+        return total;
     }
 };
