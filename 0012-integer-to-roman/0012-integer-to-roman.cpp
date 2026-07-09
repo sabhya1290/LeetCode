eb80bbfddf4ca819458ff478f8b5ec77e8d1c7c7
Sabhya Singh <sabhya1290@gmail.com>
2026-07-10 01:13:58 +0530

    Time: 4 ms (50.05%), Space: 9.2 MB (88.3%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        static vector<int> val{1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        static vector<string> sym{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        string result ="";
-        for(int i =0; i<13;i++){
-            if (num==0){
-                break;
-            }
-            int times = num/val[i];
-            while(times--){
-                result +=sym[i];
-            }
-            num = num%val[i];
-        }
-        return result;
-
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
         
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
