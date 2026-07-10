af5ac45d92b4d452ebdf8792a813c87123854a29
Sabhya Singh <sabhya1290@gmail.com>
2026-07-10 18:55:09 +0530

    Time: 15 ms (90.1%), Space: 59.3 MB (91.09%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,19 +1,58 @@
 class MyHashMap {
-public:
-    int data[1000001];
-    MyHashMap() {
-        fill(data, data + 1000001, -1);
+    struct Node{
+        int key, val;
+        Node *next = nullptr;
+        Node(int k, int val): key(k), val(val){};
+    };
+
+    vector<Node*> v = vector<Node*>(1000, nullptr);
+
+    int hash(int a){
+        return a%1000;
     }
+public:
+    MyHashMap() {
+    }
+    
     void put(int key, int value) {
-        data[key] = value;
+        remove(key);
+        int h = hash(key);
+        if(v[h] == nullptr) v[h] = new Node(key, value);
+        else{
+            Node *temp = v[h];
+            while(temp->next) temp = temp->next;
+            temp->next = new Node(key, value);
+        }
     }
     
     int get(int key) {
-        return data[key];
+        int h = hash(key);
+        if(v[h] == nullptr) return -1;
+        Node *temp = v[h];
+        while(temp){
+            if(temp->key == key) return temp->val;
+            temp = temp->next;
+        }
+        return -1;
     }
     
-    void remove(int key){
-        data[key] = -1;
+    void remove(int key) {
+        int h = hash(key);
+        if(v[h] == nullptr) return;
+        if(v[h]->key == key){
+            v[h] = v[h]->next;
+            return;
+        }
+        if(!v[h]->next) return;
+        Node *temp = v[h]->next, *prev = v[h];
+        while(temp){
+            if(temp->key == key) {
+                prev->next = temp->next;
+                return;
+            }
+            prev = prev->next;
+            temp = temp->next;
+        }
     }
 };
 
