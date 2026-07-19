b6bc3cc6d972448d62ed39dfa05468d78617d691
Sabhya Singh <sabhya1290@gmail.com>
2026-07-19 14:16:13 +0530

    Time: 0 ms (100%), Space: 27.5 MB (54.6%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,24 +1,37 @@
-// C++ implementation
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+private:
+    unordered_map<int, int> inorder_map;
+    int preorderIdx = 0;
 public:
-    int index = 0;
-    unordered_map<int, int> map;
-
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        for (int i = 0; i < inorder.size(); ++i)
-            map[inorder[i]] = i;
-        return helper(preorder, 0, inorder.size() - 1);
+        for(int i = 0; i < inorder.size(); ++i){
+            inorder_map[inorder[i]] = i;
+        }
+
+        TreeNode* root = buildTreeHelper(preorder, 0, preorder.size() - 1);
+        return root;
     }
+    TreeNode* buildTreeHelper(vector<int>& preorder, int left, int right){
+        if(left > right) return nullptr;
 
-    TreeNode* helper(vector<int>& preorder, int start, int end) {
-        if (start > end) return nullptr;
-
-        int rootVal = preorder[index++];
+        int rootVal = preorder[preorderIdx++];
         TreeNode* root = new TreeNode(rootVal);
-        int mid = map[rootVal];
 
-        root->left = helper(preorder, start, mid - 1);
-        root->right = helper(preorder, mid + 1, end);
+        int mid = inorder_map[rootVal];
+        root->left = buildTreeHelper(preorder, left, mid - 1);
+        root->right = buildTreeHelper(preorder, mid + 1, right);
+
         return root;
     }
 };
