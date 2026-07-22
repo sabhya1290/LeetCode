94604634a946ca7229541ac3e734254ed06db00d
Sabhya Singh <sabhya1290@gmail.com>
2026-07-22 14:28:25 +0530

    Time: 3 ms (69.04%), Space: 27.3 MB (81.98%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,15 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode *Tree(vector<int>& in, int x, int y,vector<int>& po,int a,int b){
-        if(x > y || a > b)return nullptr;
-        TreeNode *node = new TreeNode(po[b]);
-        int SI = x;  
-        while(node->val != in[SI])SI++;
-        node->left  = Tree(in,x,SI-1,po,a,a+SI-x-1);
-        node->right = Tree(in,SI+1,y,po,a+SI-x,b-1);
-        return node;
+    unordered_map<int, int> mp;
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n = inorder.size();
+        int posIdx = n - 1;
+        for(int i = 0; i < n; i++){
+          mp[inorder[i]] = i;  
+        }
+        TreeNode* root = helper(postorder, 0, n - 1, posIdx);
+        return root;
     }
-    TreeNode* buildTree(vector<int>& in, vector<int>& po){
-        return Tree(in,0,in.size()-1,po,0,po.size()-1);
+    TreeNode* helper(vector<int>& postorder, int start, int end, int& posIdx){
+        if(start > end) return nullptr;
+        
+        int rootVal = postorder[posIdx--];
+        TreeNode* root = new TreeNode(rootVal);
+        int mid = mp[rootVal];
+
+        root->right = helper(postorder, mid + 1, end, posIdx);
+        root->left = helper(postorder, start, mid - 1, posIdx);
+        return root;
     }
 };
