e0843f254aec571b7ad2e9fbd7823b45bf4724d1
Sabhya Singh <sabhya1290@gmail.com>
2026-07-22 14:28:50 +0530

    Time: 2 ms (74.36%), Space: 27.4 MB (59.93%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,35 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    unordered_map<int, int> mp;
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int n = inorder.size();
-        int posIdx = n - 1;
-        for(int i = 0; i < n; i++){
-          mp[inorder[i]] = i;  
+        unordered_map<int, int> inorderMap;
+        for (int i = 0; i < inorder.size(); ++i) {
+            inorderMap[inorder[i]] = i;
         }
-        TreeNode* root = helper(postorder, 0, n - 1, posIdx);
-        return root;
+        int postIndex = postorder.size() - 1;
+        return helper(postorder, inorderMap, 0, inorder.size() - 1, postIndex);
     }
-    TreeNode* helper(vector<int>& postorder, int start, int end, int& posIdx){
-        if(start > end) return nullptr;
-        
-        int rootVal = postorder[posIdx--];
-        TreeNode* root = new TreeNode(rootVal);
-        int mid = mp[rootVal];
 
-        root->right = helper(postorder, mid + 1, end, posIdx);
-        root->left = helper(postorder, start, mid - 1, posIdx);
+private:
+    TreeNode* helper(vector<int>& postorder, unordered_map<int, int>& inorderMap, int left, int right, int& postIndex) {
+        if (left > right) return nullptr;
+        
+        int rootVal = postorder[postIndex--];
+        TreeNode* root = new TreeNode(rootVal);
+        int mid = inorderMap[rootVal];
+        
+        root->right = helper(postorder, inorderMap, mid + 1, right, postIndex);
+        root->left = helper(postorder, inorderMap, left, mid - 1, postIndex);
+        
         return root;
     }
 };
