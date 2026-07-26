7cd3da0ecca600d390f7597a6ed8ea7f5005381f
Sabhya Singh <sabhya1290@gmail.com>
2026-07-26 16:09:16 +0530

    Time: 20 ms (72.11%), Space: 27.8 MB (95.99%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
@@ -1,49 +1,101 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Codec {
 public:
 
-    // Serialize using preorder traversal
+    // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        string s;
-        preorder(root, s);
+        // string s="";
+        // if(root==nullptr) {
+        //     return s+="N,";
+        // }
+        // return s+=to_string(root->val)+","+ s+=serialize(root->left)+ s+=serialize(root->right);        
+        if(root==nullptr) return"";
+        string s="";
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode*node=q.front();
+                q.pop();
+                if(node==nullptr){
+                    s+="N,";
+                    continue;
+                }
+                else s+=to_string(node->val)+",";
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
         return s;
+        
     }
 
-    void preorder(TreeNode* root, string &s) {
-        if (!root) return;
-        s += to_string(root->val) + " ";
-        preorder(root->left, s);
-        preorder(root->right, s);
-    }
+    // Decodes your encoded data to tree.
+    // TreeNode* helper(string &data) {
+    //     // int pos=data.find(",");
+    //     // string val=data.substr(0,pos);
+    //     // data.erase(0,pos+1);
+    //     // if(val=="N") return nullptr;
+    //     // TreeNode* root=new TreeNode(stoi(val));
+    //     // root->left=helper(data);
+    //     // root->right=helper(data);
+    //     // return root;
+    //  }
+    TreeNode* deserialize(string data){
+        // return helper(data);
+        if(data.size()==0) return nullptr;
+        stringstream s(data);
+        string str;
+        getline(s,str,',');
+        TreeNode* root=new TreeNode(stoi(str));
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                getline(s,str,',');
+                if(str=="N"){
+                    node->left=nullptr;
+                }
+                else{
+                    TreeNode* lefts=new TreeNode(stoi(str));
+                    node->left=lefts;
+                    q.push(lefts);
+                }
 
-    // Deserialize
-    TreeNode* deserialize(string data) {
-        if (data.empty()) return NULL;
+                getline(s,str,',');
+                if(str=="N"){
+                    node->right=nullptr;
+                }
+                else{
+                    TreeNode* rights=new TreeNode(stoi(str));
+                    node->right=rights;
+                    q.push(rights);
+                }
 
-        vector<int> preorder;
-        stringstream ss(data);
-        int x;
-        while (ss >> x)
-            preorder.push_back(x);
-
-        int idx = 0;
-        return build(preorder, idx, INT_MIN, INT_MAX);
-    }
-
-    TreeNode* build(vector<int>& preorder, int &idx, int low, int high) {
-        if (idx == preorder.size())
-            return NULL;
-
-        int val = preorder[idx];
-
-        if (val < low || val > high)
-            return NULL;
-
-        TreeNode* root = new TreeNode(val);
-        idx++;
-
-        root->left = build(preorder, idx, low, val);
-        root->right = build(preorder, idx, val, high);
-
+            }
+        }
         return root;
-    }
+
+
+}
+    
 };
+
+// Your Codec object will be instantiated and called as such:
+// Codec* ser = new Codec();
+// Codec* deser = new Codec();
+// string tree = ser->serialize(root);
+// TreeNode* ans = deser->deserialize(tree);
+// return ans;
