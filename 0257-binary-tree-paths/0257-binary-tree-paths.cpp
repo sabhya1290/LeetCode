99ba036cd826f5ba5768ef22284c50e6c7011bf0
Sabhya Singh <sabhya1290@gmail.com>
2026-07-28 22:23:52 +0530

    Time: 4 ms (14.96%), Space: 17.4 MB (82.78%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,30 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    vector<string> paths;
 public:
-    void preorder(TreeNode* current, string path) {
-        if(!current) return;
-        path += to_string(current->val) + "->";
-        if(!current->left && !current->right) {
-            path.pop_back(); path.pop_back();
-            paths.push_back(path);
-        }
-        preorder(current->right, path);
-        preorder(current->left, path);
+    void findPath(TreeNode* node, vector<string>& ans, string temp) {
+        temp += to_string(node->val);  
+        if (node->left) findPath(node->left, ans, temp + "->");  
+        if (node->right) findPath(node->right, ans, temp + "->");
+        if (!node->left && !node->right) ans.push_back(temp);  // Add path if leaf node
     }
 
     vector<string> binaryTreePaths(TreeNode* root) {
-        preorder(root, "");
-        return paths;
+        vector<string> ans;
+        if (root) findPath(root, ans, "");  // Start traversal from the root
+        return ans;
     }
 };
