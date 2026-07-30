7c9d340ac1710820405d6b726ab4b7aee4cc98db
Sabhya Singh <sabhya1290@gmail.com>
2026-07-30 17:43:00 +0530

    Time: 4 ms (69.21%), Space: 38.3 MB (71.06%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,43 +1,19 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char, int> freq;
-        for(int i = 0; i < tasks.size(); i++){
-            freq[tasks[i]]++;
-        }
-        priority_queue<int> maxHeap;
-        for(auto& entry: freq){
-            maxHeap.push(entry.second);
-        }
-        int time = 0;
+        vector<int> freq(26,0);
+        for(char c : tasks)
+        freq[c - 'A']++;
 
-        while(!maxHeap.empty()){
-            
-            vector<int> temp;
-            int cycle = n + 1;
-            // int i = 0;
+        int maxFreq = *max_element(freq.begin(), freq.end());
+        
+        int countMax = 0;
+        for(int f : freq)
+        if(f == maxFreq)
+        countMax++;
 
-            while(cycle && !maxHeap.empty()){
-                int cnt = maxHeap.top();
-                maxHeap.pop();
-
-                cnt--;
-                if(cnt > 0)
-                temp.push_back(cnt);
-                time++;
-                cycle--;
-            }
-
-            for(int val: temp){
-                maxHeap.push(val);
-            }
-
-            if(maxHeap.empty()) break;
-
-            time += cycle;
-
-        }
-        return time;
-
+        int ans = (maxFreq - 1) * (n + 1) + countMax;
+        return max((int)tasks.size(), ans);
+        
     }
 };
