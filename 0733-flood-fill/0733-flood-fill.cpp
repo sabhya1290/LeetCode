869022215d4054f328d1f67f4e8eb3e4adc8d8d8
Sabhya Singh <sabhya1290@gmail.com>
2026-08-01 16:04:01 +0530

    Time: 0 ms (100%), Space: 18 MB (75.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
+
+    void fill(vector<vector<int>>& image, int sr, int sc, int color, int cur){
+        if(sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size()) return;
+        if(image[sr][sc] != cur) return;
+        image[sr][sc] = color;
+        fill(image, sr + 1, sc, color, cur);
+        fill(image, sr - 1, sc, color, cur);
+        fill(image, sr, sc + 1, color, cur);
+        fill(image, sr, sc - 1, color, cur);
+    }
+
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size(); int m = image[0].size();
-        queue<pair<int, int>> q;
-        int curCol = image[sr][sc];
-
-        if(curCol == color) return image;
-
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        q.push({sr, sc});
-        vector<pair< int, int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        while(!q.empty()){
-            int size = q.size();
-            while(size--){
-                auto [x, y] = q.front();
-                q.pop();
-                image[x][y] = color;
-                for(auto [dx, dy]: dirs){
-                    int i = dx + x;
-                    int j = y + dy;
-                    if(i >= 0 && i < n && j >= 0 && j < m && image[i][j] == curCol && vis[i][j] == 0){
-                        image[i][j] = color;
-                        q.push({i, j});
-                        vis[i][j] = 1;
-                    }
-                }
-            }
-        }
+        if(image[sr][sc] == color) return image;
+        fill(image, sr, sc, color, image[sr][sc]);
         return image;
     }
 };
