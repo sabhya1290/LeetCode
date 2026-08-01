981bb7a1788f978aae66a62f4c6fd504bdd483b0
Sabhya Singh <sabhya1290@gmail.com>
2026-08-01 15:53:10 +0530

    Time: 0 ms (100%), Space: 18.6 MB (14.2%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
-    {
-        int n = image.size();
-        int m = image[0].size();
-        int initial = image[sr][sc];
-        if(initial==color) return image;
-        vector<int> row = {1,-1,0,0};
-        vector<int> col = {0,0,-1,1};
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        while(!q.empty())
-        {
-            int i = q.front().first;
-            int j = q.front().second;
-            q.pop();
-            image[i][j] = color;
-            for(int k=0;k<4;k++)
-            {
-                int r=i+row[k];
-                int c=j+col[k];
-                if(r>=0 && r<n && c>=0 && c<m && image[r][c]==initial)
-                {
-                    q.push({r,c});
-                    image[r][c]=color;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size(); int m = image[0].size();
+        queue<pair<int, int>> q;
+        int curCol = image[sr][sc];
+
+        if(curCol == color) return image;
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        q.push({sr, sc});
+        vector<pair< int, int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                auto [x, y] = q.front();
+                q.pop();
+                image[x][y] = color;
+                for(auto [dx, dy]: dirs){
+                    int i = dx + x;
+                    int j = y + dy;
+                    if(i >= 0 && i < n && j >= 0 && j < m && image[i][j] == curCol && vis[i][j] == 0){
+                        image[i][j] = color;
+                        q.push({i, j});
+                        vis[i][j] = 1;
+                    }
                 }
             }
         }
         return image;
     }
-
 };
