a99572ed055c438f000d2d7d889048b65b3119a0
Sabhya Singh <sabhya1290@gmail.com>
2026-08-01 18:45:40 +0530

    Time: 19 ms (56.07%), Space: 34.6 MB (94.01%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    vector<int> DIR = {0, 1, 0, -1, 0};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
+        int rows = mat.size();
+        int cols = mat[0].size();
+        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         queue<pair<int, int>> q;
-        for (int r = 0; r < m; ++r)
-            for (int c = 0; c < n; ++c)
-                if (mat[r][c] == 0) q.emplace(r, c);
-                else mat[r][c] = -1; 
 
-        while (!q.empty()) {
-            auto [r, c] = q.front(); q.pop();
-            for (int i = 0; i < 4; ++i) {
-                int nr = r + DIR[i], nc = c + DIR[i+1];
-                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
-                mat[nr][nc] = mat[r][c] + 1;
-                q.emplace(nr, nc);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 0) {
+                    q.push({i, j});
+                } else {
+                    mat[i][j] = INT_MAX;
+                }
             }
         }
-        return mat;
+
+        while (!q.empty()) {
+            pair<int, int> cell = q.front();
+            q.pop();
+            int row = cell.first;
+            int col = cell.second;
+
+            for (pair<int, int> direction : directions) {
+                int newRow = row + direction.first;
+                int newCol = col + direction.second;
+
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && mat[newRow][newCol] > mat[row][col] + 1) {
+                    mat[newRow][newCol] = mat[row][col] + 1;
+                    q.push({newRow, newCol});
+                }
+            }
+        }
+
+        return mat;        
     }
 };
