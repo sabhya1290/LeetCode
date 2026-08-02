84aef0553b35d1602e731bb1567021f21f7ed726
Sabhya Singh <sabhya1290@gmail.com>
2026-08-02 18:13:33 +0530

    Time: 20 ms (94.07%), Space: 17.4 MB (51.56%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,33 @@
+class Solution {
+
+private:
+    vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    void dfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int i, int j){
+        vis[i][j] = 1;
+        for(auto [x, y]: dirs){
+            
+            int row = i + x;
+            int col = j + y;
+
+            if(row >= 0 && row < grid.size() && col >= 0 && col < grid[0].size() && grid[row][col] == '1' && vis[row][col] == 0){
+                dfs(grid, vis, row, col);
+            }
+        }
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size(); int m = grid[0].size();
+        vector<vector<int>> vis(n ,vector<int>(m, 0));
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    cnt++;
+                    dfs(grid, vis, i , j);
+                }
+            }
+        }
+        return cnt;
+    }
+};
