e3af9c6a0fe5836677c1a6e770055c8c63bd7caf
Sabhya Singh <sabhya1290@gmail.com>
2026-08-02 18:14:19 +0530

    Time: 26 ms (62.84%), Space: 17.5 MB (42.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,13 +3,13 @@ class Solution {
 private:
     vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     void dfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int i, int j){
-        vis[i][j] = 1;
+        grid[i][j] = '0';
         for(auto [x, y]: dirs){
             
             int row = i + x;
             int col = j + y;
 
-            if(row >= 0 && row < grid.size() && col >= 0 && col < grid[0].size() && grid[row][col] == '1' && vis[row][col] == 0){
+            if(row >= 0 && row < grid.size() && col >= 0 && col < grid[0].size() && grid[row][col] == '1' ){
                 dfs(grid, vis, row, col);
             }
         }
@@ -22,7 +22,7 @@ public:
         int cnt = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(grid[i][j] == '1' && !vis[i][j]){
+                if(grid[i][j] == '1' ){
                     cnt++;
                     dfs(grid, vis, i , j);
                 }
