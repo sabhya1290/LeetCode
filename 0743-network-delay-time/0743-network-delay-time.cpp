d09b3e9084bd6f5fadb1ef596ba9deca231b09e3
Sabhya Singh <sabhya1290@gmail.com>
2026-08-06 16:49:11 +0530

    Time: 80 ms (67.74%), Space: 44.2 MB (90.61%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> adj(n + 1);
-        for(auto it: times){
-            adj[it[0]].push_back({it[1], it[2]});
+        vector<vector<pair<int,int>>> adj(n+1);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<int> result(n+1,INT_MAX);
+
+        for(auto &t: times){
+            int u = t[0];
+            int v = t[1];
+            int w = t[2];
+            adj[u].push_back({v,w});
         }
+        result[k] = 0;
+        pq.push({0,k});
 
-        vector<int> dis(n + 1, INT_MAX);
-        priority_queue<pair<int, int>, vector<pair<int, int>> , greater<pair<int, int>>> q;
-        q.push({0, k});
-        dis[k] = 0;
-
-        while(!q.empty()){
-            auto [time, node] = q.top();
-            q.pop();
-
-            if(time > dis[node]) continue;
-
-            for(auto it: adj[node]){
-
-                int nextNode = it.first;
-                int wt = it.second;
-                
-                if(dis[nextNode] > wt + time){
-                    dis[nextNode] = wt + time;
-                    q.push({dis[nextNode], nextNode});
+        while(!pq.empty()){
+            int d = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            for(auto &v: adj[node]){
+                int adjn = v.first;
+                int wt = v.second;
+                if((d+wt) < result[adjn]){
+                    result[adjn] = d+wt;
+                    pq.push({d+wt,adjn});
                 }
-
             }
         }
+        int ans = 0;
+        for(int i=1; i<=n; i++){
+            if(result[i] == INT_MAX){
+                return -1;
+            }
+            ans = max(ans,result[i]);
 
-        int maxTime = 0;
-        for(int i = 1; i < n + 1; i++){
-            if(dis[i] == INT_MAX) return -1;
-            maxTime = max(maxTime, dis[i]); 
         }
-        return maxTime;
+        return ans;
+
+        
     }
 };
