39794b52b721097f86ebadae78c3b2cbbddf35af
Sabhya Singh <sabhya1290@gmail.com>
2026-08-10 18:05:07 +0530

    Time: 0 ms (100%), Space: 19.5 MB (71.6%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,26 +1,60 @@
-class Solution {
+class UF{
+    vector<int> parent, size;
 public:
-    void dfs(vector<vector<int>>& isConnected, vector<bool>& vis, int i){
-        vis[i] = true;
-        for(int it = 0; it < isConnected.size(); it++){
-            if(isConnected[i][it] == 1 && !vis[it]){
-                dfs(isConnected, vis, it);
-            }
+    UF(int n){
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for(int i = 0; i < n + 1 ; i++){
+            parent[i] = i;
+            size[i] = 1;
         }
     }
 
+    int findP(int node){
+        if(parent[node] == node){
+            return node;
+        }
+        return parent[node] = findP(parent[node]);
+
+    }
+
+    bool unionBysize(int u, int v){
+        int pu = findP(u);
+        int pv = findP(v);
+
+        if(pu == pv) return false;
+
+        if(size[pu] < size[pv]){
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else{
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         if(isConnected.empty()) return 0;
+        
         int n = isConnected.size();
-        int province = 0;
-        vector<bool> vis(n, false);
+        UF ufs(n);
+        int province = n;
 
-        for(int i = 0; i < n; ++i){
-            if(!vis[i]){
-                province++;
-                dfs(isConnected, vis, i);
+        for(int u = 0; u < n; u++){
+            for(int v = u + 1; v < n; v++){
+                if(isConnected[u][v] == 1){
+                    if(ufs.unionBysize(u, v)) province--;
+                }
             }
         }
+        
+
+
         return province;
     }
 };
