baa6af0df532c0f09d0b1b64f7c6ae0e58d1d544
Sabhya Singh <sabhya1290@gmail.com>
2026-08-10 18:31:04 +0530

    Time: 0 ms (100%), Space: 42.1 MB (82.61%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
-    vector<int>parent;
-    vector<int>rank;
-    int find(int x){
-        if(x == parent[x])
-           return x;
-        return parent[x] = find(parent[x]);
-        
+    int findParent(int node, vector<int>& parent) {
+        if (node == parent[node]) return node;
+        return parent[node] = findParent(parent[node], parent);
+    }
 
-        
-    }
-    void Union(int x,int y){
-        int x_parent = find(x);
-        int y_parent = find(y);
-        if(x_parent == y_parent)
-            return;
-        if(rank[x_parent] > rank[y_parent]){
-            parent[y_parent] = x_parent;
-        }else if(rank[x_parent] < rank[y_parent]){
-            parent[x_parent] = y_parent;
-        }else{
-            parent[x_parent] = y_parent;
-            rank[y_parent]++;
-        }
-    }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size() < n-1)
-            return -1;
-        parent.resize(n);
-        rank.resize(n,0);
-        for(int i =0;i<n;i++){
-            parent[i] = i;
-        }
-        int component = n;
-        for(auto &vec : connections){
-            if(find(vec[0]) != find(vec[1])){
-                Union(vec[0],vec[1]);
-                component--;
+        if (connections.size() < n - 1) return -1;
+
+        vector<int> parent(n);
+        vector<int> size(n, 1);
+        
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+        int components = n;
+
+        for (const auto& edge : connections) {
+            int pu = findParent(edge[0], parent);
+            int pv = findParent(edge[1], parent);
+
+            if (pu != pv) {
+                if (size[pu] < size[pv]) {
+                    parent[pu] = pv;
+                    size[pv] += size[pu];
+                } else {
+                    parent[pv] = pu;
+                    size[pu] += size[pv];
+                }
+                components--; 
             }
         }
-        return component-1;
-        
+
+        return components - 1;
     }
 };
