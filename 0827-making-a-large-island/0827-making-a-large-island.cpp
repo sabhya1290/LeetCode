513929f9fa20b803d84048bdf6de098ce19e762b
Sabhya Singh <sabhya1290@gmail.com>
2026-08-13 00:01:56 +0530

    Time: 151 ms (96.5%), Space: 101.4 MB (94.22%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,38 +1,77 @@
 class Solution {
 public:
-    const int DIR[5] = {0, 1, 0, -1, 0};
-    int m, n;
-    unordered_map<int, int> componentSize;
-    int largestIsland(vector<vector<int>>& grid) {
-        m = grid.size(); n = grid[0].size();
-        int ans = 0, nextColor = 2;
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
-                if (grid[r][c] != 1) continue; 
-                paint(grid, r, c, nextColor++);
-                ans = max(ans, componentSize[nextColor - 1]);
-            }
-        }
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
-                if (grid[r][c] != 0) continue; 
-                unordered_set<int> neighborColors;
-                for (int i = 0; i < 4; ++i) {
-                    int nr = r + DIR[i], nc = c + DIR[i+1];
-                    if (nr < 0 || nr == m || nc < 0 || nc == n || grid[nr][nc] == 0) continue;
-                    neighborColors.insert(grid[nr][nc]);
-                }
-                int sizeFormed = 1;
-                for (int color : neighborColors) sizeFormed += componentSize[color];
-                ans = max(ans, sizeFormed);
-            }
-        }
-        return ans;
+    int n;
+    int dfs(vector<vector<int>>& grid, int r, int c, int id) {
+        if (r < 0 || r >= n || c < 0 || c >= n)
+            return 0;
+
+        if (grid[r][c] != 1)
+            return 0;
+
+        grid[r][c] = id;
+        int size = 1;
+        size += dfs(grid, r + 1, c, id);
+        size += dfs(grid, r - 1, c, id);
+        size += dfs(grid, r, c + 1, id);
+        size += dfs(grid, r, c - 1, id);
+
+        return size;
     }
-    void paint(vector<vector<int>>& grid, int r, int c, int color) {
-        if (r < 0 || r == m || c < 0 || c == n || grid[r][c] != 1) return;
-        grid[r][c] = color;
-        componentSize[color] += 1;
-        for (int i = 0; i < 4; ++i) paint(grid, r + DIR[i], c + DIR[i+1], color);
+
+    int largestIsland(vector<vector<int>>& grid) {
+
+        n = grid.size();
+        unordered_map<int, int> islandsize;
+        int id = 2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == 1) {
+                    int size = dfs(grid, i, j, id);
+                    islandsize[id] = size;
+                    id++;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != 0)
+                    continue;
+
+                set<int> uniqueislands;
+
+                if (i > 0 && grid[i - 1][j] > 1) {
+                    uniqueislands.insert(grid[i - 1][j]);
+                }
+
+                if (i + 1 < n && grid[i + 1][j] > 1) {
+                    uniqueislands.insert(grid[i + 1][j]);
+                }
+
+                if (j > 0 && grid[i][j - 1] > 1) {
+                    uniqueislands.insert(grid[i][j - 1]);
+                }
+
+                if (j + 1 < n && grid[i][j + 1] > 1) {
+                    uniqueislands.insert(grid[i][j + 1]);
+                }
+
+                int currsize = 1;
+                for (int islands : uniqueislands) {
+                    currsize += islandsize[islands];
+                }
+
+                ans = max(ans, currsize);
+            }
+        }
+
+        if (ans == 0) {
+            return n * n;
+            
+        }
+
+        return ans;
     }
 };
