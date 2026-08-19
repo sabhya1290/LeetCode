a12ed1d90b82bba63c6204bbd0f320cf94b02c4b
Sabhya Singh <sabhya1290@gmail.com>
2026-08-19 17:46:17 +0530

    Time: 0 ms (100%), Space: 10.1 MB (90.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -22,18 +22,32 @@ public:
 
         if (n == 1) return nums[0];
 
-        vector<int> dp(n, -1);
+        // vector<int> dp(n, -1);
 
-        dp[0] = nums[0];
-        dp[1] = max(nums[1], nums[0]);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[1], nums[0]);
 
         // Tabulation
-        for(int i = 2; i < n; i++){
-            int pick = nums[i] + dp[i - 2];
-            int notpick = dp[i - 1];
+        // for(int i = 2; i < n; i++){
+        //     int pick = nums[i] + dp[i - 2];
+        //     int notpick = dp[i - 1];
 
-            dp[i] = max(pick, notpick);
+        //     dp[i] = max(pick, notpick);
+        // }
+        // return dp[n - 1];
+
+// Space Optimization
+        int prev = nums[0];
+        int prev2 = max(nums[0], nums[1]);
+
+        for(int i = 2; i < n; i++){
+            int pick = nums[i] + prev;
+            int notp = prev2;
+
+            int cur = max(pick, prev2);
+            prev = prev2;
+            prev2 = cur;
         }
-        return dp[n - 1];
+        return prev2;
     }
 };
