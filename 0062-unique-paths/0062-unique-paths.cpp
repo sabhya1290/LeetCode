9c1a73fd1e23892cc6a902ed2dda18dbcdb9a607
Sabhya Singh <sabhya1290@gmail.com>
2026-09-12 18:46:09 +0530

    Time: 0 ms (100%), Space: 8.2 MB (87.51%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    int rec(int m, int n, vector<vector<int>> &dp){
-        if(m == 0 && n == 0) return 1;
-        if(m < 0 || n < 0) return 0;
-        if(dp[m][n] != -1) return dp[m][n];
-        int r = rec(m - 1, n, dp);
-        int d = rec(m, n - 1, dp);
-        return dp[m][n] = r + d;
-    }
     int uniquePaths(int m, int n) {
-        // vector<vector<int>> dp(m, vector<int>(n, 0)); 
-        // int ans = rec(m - 1, n - 1, dp);
-        // dp[0][0] = 1;
 
         vector<int> prev(n, 0);
+
         for(int i = 0; i < m; i++){
+
             vector<int> temp(n, 0);
+
             for(int j = 0; j < n; j++){
+
                 if(i == 0 && j == 0) temp[j] = 1;
+
                 else{
-                    // int up = 0, left = 0;
-                    // if(i > 0) up = dp[i - 1][j];
-                    // if(j > 0) left = dp[i][j - 1];
-                    // dp[i][j] = left + up;
+                   
                     int up = 0, left = 0;
                     if(i > 0) up = prev[j];
                     if(j > 0) left = temp[j - 1];
                     temp[j] = up + left;
+                    
                 }
             }
             prev = temp;
